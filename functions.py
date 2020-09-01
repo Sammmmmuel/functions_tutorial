ad7e5a57d60ed089cdbba2f4a04b08efa1a91a18
Samuel Madrigal <Samuel.madriigal@gmail.com>
2020-09-01 12:38:50 -0700

    message

---
+++ functions.py
@@ -0,0 +1,40 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result1 = area(3, 3)
+result2 = area(1, 1)
+
+print(result1)
+print(result2)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+sum1 = subtract(5, 1)
+sum2 = subtract(4, 1)
+sum3 = subtract(3, 1)
+
+print(sum1)
+print(sum2)
+print(sum3)
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+product = divide(6, 2)
+print(product)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+answer = add(2, 2)
+answer2 = add(3, 3)
+print(answer)
+print(answer2)
